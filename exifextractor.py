3d526ad272468fff50a15be8e1f94eff9dcbb57d
Amraj Randhawa <randha13@my.yorku.ca>
2024-07-26 22:04:53 -0400

    exif data stores cords in deg, min, secs format so we convert to decimal degrees.

--- exifextractor.py
+++ exifextractor.py
@@ -1,1 +1,15 @@
-from exif import Image
+import os
+import sys
+from PIL import Image
+from PIL.ExifTags import TAGS, GPSTAGS
+
+#decimal format conversion
+
+def conversion_decimal(dms, reference):
+    degrees, minutes, seconds = dms
+    decimal = degrees[0] / degrees[1] + \
+              minutes[0] / (minutes[1] * 60.0) + \
+              seconds[0] / (seconds[1] * 3600.0)
+    if reference in ['S', 'W']:
+        decimal = -decimal
+    return decimal
