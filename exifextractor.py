f87499e51352e388a31cb001dd94f3d05aa00ad4
Amraj Randhawa <randha13@my.yorku.ca>
2024-07-26 23:29:20 -0400

    bugfixing tags, implemented full program to find location using long and latitude.

--- exifextractor.py
+++ exifextractor.py
@@ -6,10 +6,36 @@ from PIL.ExifTags import TAGS, GPSTAGS
 #decimal format conversion
 
 def conversion_decimal(dms, reference):
-    degrees, minutes, seconds = dms
-    decimal = degrees[0] / degrees[1] + \
-              minutes[0] / (minutes[1] * 60.0) + \
-              seconds[0] / (seconds[1] * 3600.0)
+    degrees = dms[0].numerator / dms[0].denominator
+    minutes = dms[1].numerator / dms[1].denominator
+    seconds = dms[2].numerator / dms[2].denominator
+
+    decimal = degrees + (minutes / 60.0) + (seconds / 3600.0)
     if reference in ['S', 'W']:
         decimal = -decimal
     return decimal
+
+# Gathering EXIF information from images.
+with Image.open("/Users/Amraj/Desktop/IMG_6152.jpg") as img:
+    exif_data = img._getexif()
+    gps_data = {}
+
+    #if it has exif data
+    if exif_data:
+        #get gps data
+    
+        for tag_id in exif_data:
+            #get tags name
+            tag = TAGS.get(tag_id, tag_id)
+            if tag == 'GPSInfo':
+                for key, val in exif_data[tag_id].items():
+                    decoded = GPSTAGS.get(key, key)
+                    gps_data[decoded] = val
+
+    #check if contains latitude and longitude
+    if 'GPSLatitude' in gps_data and 'GPSLongitude' in gps_data:
+        lat = conversion_decimal(gps_data['GPSLatitude'], gps_data['GPSLatitudeRef'])
+        long = conversion_decimal(gps_data['GPSLongitude'], gps_data['GPSLongitudeRef'])
+        print(f"Latitude: {lat}, Longitude: {long}")
+    else:
+            print("No GPS data available.")
