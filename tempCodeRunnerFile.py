85ec0b75644ca160e23379eb12101ff4f8d83ed6
Amraj Randhawa <randha13@my.yorku.ca>
2024-11-14 21:47:06 -0500

    deleted

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-def info_data(filepath):
-            
-        with Image.open(filename) as img:
-            exif_data = img._getexif()
-            if exif_data:
-                print("EXIF Data Located!")
-                exif = {TAGS.get(k, k): v for k, v in exif_data.items()}
-                print(exif)
